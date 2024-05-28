a820744cbc21414f1439ab364e39b63d0e59653b
JakubPilarski <250991@edu.p.lodz.pl>
2024-05-28 17:04:37 +0200

    dalej

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include "class.h"
 using namespace std;
-int main() {
-    Triangle t1 = Triangle("T1",3,4);
+int main() {double ts1;
+    double ts2;
+    cout << "First adjoining our your triangle =  ";
+    cin >> ts1;
+    cout << endl;
+    cout << "Second adjoining our your triangle =  ";
+    cin >> ts2;
+    cout << endl;
+    Triangle t1("T1",3,4);
     t1.showSides();
     t1.showPerimeter();
     t1.showArea();
