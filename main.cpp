15f1649dc26a887dc9a58c0f71d51d8eff07b7c6
JakubPilarski <250991@edu.p.lodz.pl>
2024-05-28 21:15:42 +0200

    Squer

--- main.cpp
+++ main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include "class.h"
 using namespace std;
-int main() {double ts1;
-    double ts2;
+int main() {
+    double x;
+    double y;
+    cout << "Triangle" << endl;
     cout << "First adjoining our your triangle =  ";
-    cin >> ts1;
+    cin >> x;
     cout << endl;
     cout << "Second adjoining our your triangle =  ";
-    cin >> ts2;
+    cin >> y;
     cout << endl;
-    Triangle t1("T1",3,4);
+    Triangle t1("T1",x,y);
     t1.showSides();
     t1.showPerimeter();
     t1.showArea();
+    cout << endl;
+    cout << "Rectangle" << endl;
+    cout << "First side our your rectangle =  ";
+    cin >> x;
+    cout << endl;
+    cout << "Second side our your rectangle =  ";
+    cin >> y;
+    cout << endl;
+    Rectangle r1("R1",x,y);
+    r1.showSidesRe();
+    r1.showPerimeterRe();
+    r1.showAreaRe();
     return 0;
 }
